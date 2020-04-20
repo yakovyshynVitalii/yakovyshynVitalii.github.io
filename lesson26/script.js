cd79400bbcd2879c41bccefe63fb6df8ee1a8e58
yakovyshynVitalii <vakovisin94@gmail.com>
2020-04-20 16:54:05 +0300

    start lesson26

---
+++ lesson26/script.js
@@ -0,0 +1,15 @@
+car = {
+    manufacturer : 'Ford',
+    model : 'mustang_gt',
+    yaerOfIssue: 2018,
+    averageSpeed: 100,
+}
+
+function myCar (){
+ console.log (`car manufacturer ${car.manufacturer},
+ model ${car.model},
+ year of issue ${car.yaerOfIssue},
+ average speed ${car.averageSpeed}.`);
+}
+
+myCar ();
