ebcd7e04129703970e713fc75e797f319e4c2632
yakovyshynVitalii <vakovisin94@gmail.com>
2020-06-18 19:13:22 +0300

    finished shop

--- last_exam/Shop/js/shop.js
+++ last_exam/Shop/js/shop.js
@@ -1,10 +1,34 @@
-function init() {
-    $.getJSON("goods.json",goodsOut);
-}
 
-function goodsOut(){
-    //загружаю товари на сторінку
-    console.log(data);
-  
+const Album = {
+    "11292" : {
+        "name":"CD(not to disappear)",
+        "cost": 55,
+        "description": "Release date: 15th january 2016",
+        "image":"../img/item1.png"
+        }
 }
-init();
+let caount = 0;
+
+
+$('#add_cart').click(function(){
+    // const cart = JSON.parse (localStorage.getItem('cart'));
+    
+    // if(cart){
+    //     localStorage.setItem('cart',JSON.stringify([...cart,"11292"]));
+    // }
+    caount++;
+
+    $('#count').text(caount);
+});
+
+
+$('#subtract').click(function(){
+    // const cart = JSON.parse (localStorage.getItem('cart'));
+    
+    // if(cart){
+    //     localStorage.setItem('cart',JSON.stringify([...cart,"11292"]));
+    // }
+    caount--;
+
+    $('#count').text(caount);
+});
